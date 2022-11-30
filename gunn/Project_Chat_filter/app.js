69606b0e4686ad918adc1ca444198054f71c0cdd
Wizat <passavit1@gmail.com>
2022-11-30 21:07:36 +0700

    Project Done

--- gunn/Project_Chat_filter/app.js
+++ gunn/Project_Chat_filter/app.js
@@ -1,50 +1,56 @@
-import { data } from "./FetchData.js";
-
-const profile_pic = document.getElementById("profile_pic");
-const user_list = document.getElementById("user_list"); //ul
-
-profile_pic.src = `${data.results[0].picture.thumbnail}`;
-
-// const setUser = (item) => {
-//   console.log(item);
-//   let chat_show = document.getElementById("chat_container");
-// chat_show.innerHTML = `<img src="${item.picture.large}"  alt="please keep phone"/>`;
-// chat_show.innerHTML += `<h2>${item.name.title}.${item.name.first} ${item.name.last}</h2>`;
-// chat_show.innerHTML += `<span>Gender: ${item.gender},  Age: ${item.dob.age}</span>`;
-// chat_show.innerHTML += `<span>Email: ${item.email}</span>`;
-// chat_show.innerHTML += `<span class='location'><img width='20' src="./image/phone.svg"  alt="phone"/> ${item.phone}</span>`;
-// chat_show.innerHTML += `<span class='location'><img width='20' src="./image/location.svg"  alt="location"/> ${item.location.city}, ${item.location.country}</span>`;
-// };
-
-const generateList = (userlist) => {
-  userlist.forEach((x) => {
-    let li = document.createElement("li");
-    li.className = "user_list_detail";
-    li.innerHTML = `<img src = '${x.picture.thumbnail}' alt = '' /> <div class = 'user_list_detail_contact'><div>${x.name.title} ${x.name.first} ${x.name.last}</div><div>${x.email}</div></div>`;
-    user_list.appendChild(li);
-    // li.addEventListener("click", setUser(x));
+const url = "https://randomuser.me/api/?results=24";
+const getUser = async () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const json = await (await fetch(url)).json();
+      resolve(json);
+    } catch (error) {
+      reject(error);
+    }
   });
 };
 
-const main = () => {
-  generateList(data.results);
-  if (data?.results) {
-    generateList(data.results);
-    let rawUser = data.results;
-    let searchUser = document.getElementById("search-box-input");
-    searchUser.addEventListener("input", (event) => {
+const main = async () => {
+  let runner = await getUser();
+  if (runner?.results) {
+    genList(runner.results);
+    let rawData = runner.results;
+    let search_box = document.getElementById("search-box-input");
+    search_box.addEventListener("input", (event) => {
       let search = event.target.value;
-      let user_list = document.getElementById("user_list"); //ul
-      user_list.innerHTML = "";
-      let newData = rawUser.filter((item) => {
-        let name = `${item.name.title}.${item.name.first} ${item.name.last} `;
+      let userContainer = document.getElementById("user_list");
+      userContainer.innerHTML = "";
+      let newData = rawData.filter((x) => {
+        let name = `${x.name.title}.${x.name.first} ${x.name.last} `;
         return name.toLowerCase().includes(search.toLowerCase());
       });
-      generateList(newData);
+      genList(newData);
     });
-  } else {
-    console.log("error");
   }
 };
 
 main();
+
+const genList = (userList) => {
+  let userContainer = document.getElementById("user_list");
+  userList.forEach((x) => {
+    let li = document.createElement("li");
+    li.className = "user_list_detail";
+    li.innerHTML = `<img src = '${x.picture.medium}' alt = '${x.name.first}' /> <div class = 'user_list_detail_contact'><div>${x.name.title}.${x.name.first} ${x.name.last} </div><div>${x.email}</div></div> `;
+    userContainer.appendChild(li);
+    li.addEventListener("click", () => {
+      setUser(x);
+    });
+  });
+};
+
+const setUser = (x) => {
+  console.log(x);
+  let showUser = document.getElementById("chat_container");
+  showUser.innerHTML = `<img src= '${x.picture.large}' />`;
+  showUser.innerHTML += `<h2>${x.name.title}.${x.name.first} ${x.name.last}</h2>`;
+  showUser.innerHTML += `<p>Gender: ${x.gender}, Age: ${x.dob.age}</p>`;
+  showUser.innerHTML += `<p>Email: ${x.email}</p>`;
+  showUser.innerHTML += `<p>Tel: ${x.phone}</p>`;
+  showUser.innerHTML += `<p>Address: ${x.location.state}, ${x.location.country}</p>`;
+};
